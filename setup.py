fc10241f665e19af1fb8306d0cd039eac87ae4b9
Sagi Shnaidman <sshnaidm@redhat.com>
2020-08-06 14:04:57 +0300

    Add setup files for Pip installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Copyright Red Hat, Inc. All Rights Reserved.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
