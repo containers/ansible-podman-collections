d7cdfa549e66639f780e86f86a6da9e1a6772d39
Sagi Shnaidman <sshnaidm@redhat.com>
2021-04-13 13:02:45 +0300

    Fix overlayfs issue in CI for buildah connection

--- tests/integration/targets/connection_buildah/runme.sh
+++ tests/integration/targets/connection_buildah/runme.sh
@@ -12,11 +12,16 @@ function run_ansible {
 
 }
 
+# Issue in buildah: https://github.com/containers/buildah/issues/3126
+# Hack is from: https://github.com/containers/buildah/issues/3120#issuecomment-815889314
+# PR is merged here: https://github.com/containers/storage/pull/871
+export STORAGE_OPTS="overlay.mount_program=/usr/bin/fuse-overlayfs"
 # First run as root
 run_ansible "$@"
 
-# Create a normal user
-${SUDO:-} ansible all -i "test_connection.inventory" -m "user" -a 'name="testuser"'
+# Create a non-root user
+${SUDO:-} ${ANSIBLECMD:-ansible-playbook} -i "test_connection.inventory" ../connection/create-nonroot-user.yml \
+    -e target_hosts="buildah"
 
 # Second run as normal user
 ANSIBLE_VERBOSITY=4 ANSIBLE_REMOTE_USER="testuser" run_ansible "$@" | tee check_log
