2577f5b0f516dd0657a91a722bac4dc810803062
Sergey <6213510+sshnaidm@users.noreply.github.com>
2022-06-12 22:54:03 +0300

    Remove distutils as deprecated (#437)
    Fix #422
    The distutils package is deprecated and slated for removal in Python 3.12
    
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

--- plugins/module_utils/podman/podman_pod_lib.py
+++ plugins/module_utils/podman/podman_pod_lib.py
@@ -1,9 +1,8 @@
 from __future__ import (absolute_import, division, print_function)
 import json
-from distutils.version import LooseVersion
 
 from ansible.module_utils._text import to_bytes, to_native
-
+from ansible_collections.containers.podman.plugins.module_utils.podman.common import LooseVersion
 from ansible_collections.containers.podman.plugins.module_utils.podman.common import lower_keys
 from ansible_collections.containers.podman.plugins.module_utils.podman.common import generate_systemd
 
