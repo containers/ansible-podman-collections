04eb1da6efd5ff51d9ea2c086a1c76a0ef46bdb5
Sagi Shnaidman <sshnaidm@redhat.com>
2022-09-02 22:52:06 +0300

    Support passing multiple networks with params
    Fix #467
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

--- plugins/module_utils/podman/podman_container_lib.py
+++ plugins/module_utils/podman/podman_container_lib.py
@@ -481,6 +481,10 @@ class PodmanModuleParams:
         return c
 
     def addparam_network(self, c):
+        if LooseVersion(self.podman_version) >= LooseVersion('4.0.0'):
+            for net in self.params['network']:
+                c += ['--network', net]
+            return c
         return c + ['--network', ",".join(self.params['network'])]
 
     def addparam_network_aliases(self, c):
