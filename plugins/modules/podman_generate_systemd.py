db52d3aad95f6669bb0cfdf4ad9a6843c354dbd6
Sébastien Gendre <seb@k-7.ch>
2022-10-21 08:25:45 +0200

    Start a new Ansible module: podman_generate_systemd
    This module, when finished, will let user generate a systemd .service
    unit file from a pod or a container.
    
    If a pod is specified, then the .service file is made for the pod and
    its containers.
    
    This commit is only the beginning, with two empty functions and the
    base for a Python script.

---
+++ plugins/modules/podman_generate_systemd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# 2022, Sébastien Gendre <seb@k-7.ch>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+# TODO: Write DOCUMENTATION, EXAMPLES and RETURN
+
+DOCUMENTATION = '''
+'''
+
+EXAMPLES = '''
+'''
+
+RETURN = '''
+'''
+
+import os
+
+def generate_systemd():
+    """Generate systemd .service unit file from a pod or container"""
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
