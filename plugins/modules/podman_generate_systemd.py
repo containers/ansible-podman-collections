393f6a85688515c89144279866324f0296a862dc
Sébastien Gendre <seb@k-7.ch>
2022-10-22 19:03:40 +0200

    podman_generate_systemd : Remove not needed import from __future__

--- plugins/modules/podman_generate_systemd.py
+++ plugins/modules/podman_generate_systemd.py
@@ -2,7 +2,6 @@
 
 # 2022, Sébastien Gendre <seb@k-7.ch>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import absolute_import, division, print_function
 
 # TODO: Write DOCUMENTATION, EXAMPLES and RETURN
 
