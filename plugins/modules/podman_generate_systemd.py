2d7cadaa0477f5aee7b1c38b898d2b2d3a6120d6
Sébastien Gendre <seb@k-7.ch>
2022-10-23 10:19:01 +0200

    podman_generate_systemd: Remove TODO

--- plugins/modules/podman_generate_systemd.py
+++ plugins/modules/podman_generate_systemd.py
@@ -3,8 +3,6 @@
 # 2022, Sébastien Gendre <seb@k-7.ch>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# TODO: Write DOCUMENTATION, EXAMPLES and RETURN
-
 DOCUMENTATION = '''
 module: podman_generate_systemd
 author:
