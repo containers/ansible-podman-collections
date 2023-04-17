57a1a77c712eca39f56d81ad4cacd86100895b14
Holger Mueller <holger.mueller@inovex.de>
2023-04-17 17:03:05 +0200

    Don't recreate if CPU limts are default fixes #559

--- plugins/module_utils/podman/podman_container_lib.py
+++ plugins/module_utils/podman/podman_container_lib.py
@@ -741,7 +741,7 @@ class PodmanContainerDiff:
         return self._diff_update_and_compare('annotation', before, after)
 
     def diffparam_env_host(self):
-        # It's impossible to get from inspest, recreate it if not default
+        # It's impossible to get from inspect, recreate it if not default
         before = False
         after = self.params['env_host']
         return self._diff_update_and_compare('env_host', before, after)
@@ -830,12 +830,18 @@ class PodmanContainerDiff:
 
     def diffparam_cpu_period(self):
         before = self.info['hostconfig']['cpuperiod']
-        after = self.params['cpu_period']
+        # if cpu_period left to default keep settings
+        after = self.params['cpu_period'] or before
         return self._diff_update_and_compare('cpu_period', before, after)
 
     def diffparam_cpu_quota(self):
         before = self.info['hostconfig']['cpuquota']
+<<<<<<< HEAD
         after = self.params['cpu_quota']
+=======
+        # if cpu_quota left to default keep settings
+        after = self.params['cpu_quota'] or before
+>>>>>>> c18ca33 (Don't recreate if CPU limts are default fixes #559)
         return self._diff_update_and_compare('cpu_quota', before, after)
 
     def diffparam_cpu_rt_period(self):
@@ -855,7 +861,8 @@ class PodmanContainerDiff:
 
     def diffparam_cpus(self):
         before = int(self.info['hostconfig']['nanocpus']) / 1000000000
-        after = self.params['cpus']
+        # if cpus left to default keep settings
+        after = self.params['cpus'] or before
         return self._diff_update_and_compare('cpus', before, after)
 
     def diffparam_cpuset_cpus(self):
@@ -1155,7 +1162,8 @@ class PodmanContainerDiff:
                     before.append(compose(port, h))
         after = self.params['publish'] or []
         if self.params['publish_all']:
-            image_ports = self.image_info.get('config', {}).get('exposedports', {})
+            image_ports = self.image_info.get(
+                'config', {}).get('exposedports', {})
             if image_ports:
                 after += list(image_ports.keys())
         after = [
