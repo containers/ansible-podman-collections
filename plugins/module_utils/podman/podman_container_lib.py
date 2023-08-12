faee18ecdc40c6e5fde36c37947d060774693330
Sergey <6213510+sshnaidm@users.noreply.github.com>
2023-08-12 11:32:39 +0300

    Fix healthcheck issue where defined in image (#618)
    Fix #598
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

--- plugins/module_utils/podman/podman_container_lib.py
+++ plugins/module_utils/podman/podman_container_lib.py
@@ -943,12 +943,12 @@ class PodmanContainerDiff:
     # Healthcheck is only defined in container config if a healthcheck
     # was configured; otherwise the config key isn't part of the config.
     def diffparam_healthcheck(self):
+        before = ''
         if 'healthcheck' in self.info['config']:
             # the "test" key is a list of 2 items where the first one is
             # "CMD-SHELL" and the second one is the actual healthcheck command.
-            before = self.info['config']['healthcheck']['test'][1]
-        else:
-            before = ''
+            if len(self.info['config']['healthcheck']['test']) > 1:
+                before = self.info['config']['healthcheck']['test'][1]
         after = self.params['healthcheck'] or before
         return self._diff_update_and_compare('healthcheck', before, after)
 
