5265f8c91c0939f6e8af8954781ee05af89fb0b0
Sergey <6213510+sshnaidm@users.noreply.github.com>
2025-08-25 10:25:00 +0300

    Test new documentation (#972)
    Signed-off-by: Sagi Shnaidman <sshnaidm@redhat.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,12 +1,11 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
